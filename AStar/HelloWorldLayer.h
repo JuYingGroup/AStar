5de1e504d080d8f99e65f9d58e381f69b5507e69
caodi789 <100061501@qq.com>
2013-06-28 16:51:29 +0800

    Initial Commit

---
+++ AStar/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  AStar
+//
+//  Created by 曹笛 on 13-6-28.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
